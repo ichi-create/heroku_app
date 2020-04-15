99885d9d4d00152276e7dacf4928171d02238a36
EC2 Default User <ec2-user@ip-172-31-41-211.us-east-2.compute.internal>
2020-04-15 16:24:15 +0000

    init

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def easy
+    
+  end
+
+  def nomal
+  end
+
+  def difficult
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
-
+  get "/" => "home#top"
+  get "/easy" => "home#easy"
+  get "/nomal" => "home#nomal"
+  get "/difficult" => "home#difficult"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
